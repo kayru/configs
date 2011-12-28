7d58740140e45699979283fed41ce97a5f8d6d78
Yuriy O'Donnell <yuriyo@gmail.com>
2011-12-28 00:31:37 +0000

    macos home/end key fix, xcode colour scheme

---
+++ macos/install.sh
@@ -0,0 +1,5 @@
+mkdir ~/Library/KeyBindings/
+cp DefaultKeyBinding.dict ~/Library/KeyBindings/
+
+mkdir ~/Library/Developer/Xcode/UserData/FontAndColorThemes
+cp Kayru.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes
