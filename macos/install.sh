0f904619a0f3aaca4ed9540201efc670653f92be
Yuriy O'Donnell <yuriyo@gmail.com>
2012-02-05 13:22:23 +0000

    macos install script tidy

--- macos/install.sh
+++ macos/install.sh
@@ -1,5 +1,7 @@
+echo "Copying key bindings to ~/Library/KeyBindings"
 mkdir -p ~/Library/KeyBindings/
 cp DefaultKeyBinding.dict ~/Library/KeyBindings/
 
+echo "Copying key xcode colour scheme to ~/Library/Developer/Xcode/UserData/FontAndColorThemes"
 mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes
 cp Kayru.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes
